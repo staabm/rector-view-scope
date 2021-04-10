266a27bc34f497da9cbc7e968e8456ccdcadbfd1
Markus Staab <markus.staab@redaxo.de>
2021-04-10 21:46:41 +0200

    draft: infer types from controller props

--- lib/ViewScopeRector.php
+++ lib/ViewScopeRector.php
@@ -11,11 +11,25 @@ use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocTagValueNode;
 use PHPStan\PhpDocParser\Ast\PhpDoc\VarTagValueNode;
 use PHPStan\PhpDocParser\Ast\Type\IdentifierTypeNode;
 use PHPStan\PhpDocParser\Ast\Type\NullableTypeNode;
+use PHPStan\PhpDocParser\Ast\Type\TypeNode;
+use PHPStan\Reflection\ReflectionProvider;
 use Rector\Core\Rector\AbstractRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\ConfiguredCodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
-class ViewScopeRector extends AbstractRector {
+class ViewScopeRector extends AbstractRector
+{
+    /**
+     * @var ReflectionProvider
+     */
+    private $reflectionProvider;
+
+    public function __construct(ReflectionProvider $reflectionProvider
+    )
+    {
+        $this->reflectionProvider = $reflectionProvider;
+    }
+
     public function getRuleDefinition(): RuleDefinition
     {
         return new RuleDefinition('Infer view scope', [new ConfiguredCodeSample('', '')]);
@@ -47,11 +61,30 @@ array(
             return null;
         }
 
+        $inferredType = $this->inferTypeFromController("\IndexController", $node->exprs[0]->name);
+        if (!$inferredType) {
+            return null;
+        }
+
         // https://github.com/rectorphp/rector/blob/main/docs/how_to_work_with_doc_block_and_comments.md
         $phpDocInfo = $this->phpDocInfoFactory->createFromNodeOrEmpty($node);
-        $phpDocInfo->addTagValueNode(new VarTagValueNode(new IdentifierTypeNode('string'), '$'. $node->exprs[0]->name, ''));
+        $phpDocInfo->addTagValueNode(new VarTagValueNode($inferredType, '$' . $node->exprs[0]->name, ''));
 
         return $node;
+    }
+
+    /**
+     * @param class-string $controllerClass
+     * @param string $propertyName
+     * @throws \PHPStan\Reflection\MissingPropertyFromReflectionException
+     */
+    private function inferTypeFromController($controllerClass, $propertyName): TypeNode
+    {
+        $classReflection = $this->reflectionProvider->getClass($controllerClass);
+        // XXX where to get the $scope from?
+        $propertyReflection = $classReflection->getProperty($propertyName);
+
+        return $propertyReflection->getReadableType();
     }
 
     /*
