2755bf6fb71f3ae17cdc7cfb36ff8e6b16d3dde2
Markus Staab <maggus.staab@googlemail.com>
2021-04-10 22:55:21 +0200

    infer types from controller props (#3)

--- lib/ViewScopeRector.php
+++ lib/ViewScopeRector.php
@@ -4,6 +4,7 @@ namespace ViewScopeRector;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr\Variable;
+use PHPStan\Analyser\Scope;
 use PHPStan\PhpDocParser\Ast\PhpDoc\ParamTagValueNode;
 use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocNode;
 use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocTagNode;
@@ -11,11 +12,26 @@ use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocTagValueNode;
 use PHPStan\PhpDocParser\Ast\PhpDoc\VarTagValueNode;
 use PHPStan\PhpDocParser\Ast\Type\IdentifierTypeNode;
 use PHPStan\PhpDocParser\Ast\Type\NullableTypeNode;
+use PHPStan\PhpDocParser\Ast\Type\TypeNode;
+use PHPStan\Reflection\ReflectionProvider;
 use Rector\Core\Rector\AbstractRector;
+use Rector\NodeTypeResolver\Node\AttributeKey;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\ConfiguredCodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
-class ViewScopeRector extends AbstractRector {
+class ViewScopeRector extends AbstractRector
+{
+    /**
+     * @var ReflectionProvider
+     */
+    private $reflectionProvider;
+
+    public function __construct(ReflectionProvider $reflectionProvider
+    )
+    {
+        $this->reflectionProvider = $reflectionProvider;
+    }
+
     public function getRuleDefinition(): RuleDefinition
     {
         return new RuleDefinition('Infer view scope', [new ConfiguredCodeSample('', '')]);
@@ -47,11 +63,37 @@ array(
             return null;
         }
 
+        $variable = $node->exprs[0];
+
+        $inferredType = $this->inferTypeFromController("\IndexController", $variable);
+        if (!$inferredType) {
+            return null;
+        }
+
         // https://github.com/rectorphp/rector/blob/main/docs/how_to_work_with_doc_block_and_comments.md
         $phpDocInfo = $this->phpDocInfoFactory->createFromNodeOrEmpty($node);
-        $phpDocInfo->addTagValueNode(new VarTagValueNode(new IdentifierTypeNode('string'), '$'. $node->exprs[0]->name, ''));
+        $phpDocInfo->addTagValueNode(new VarTagValueNode($inferredType, '$' . $variable->name, ''));
 
         return $node;
+    }
+
+    /**
+     * @param class-string $controllerClass
+     * @throws \PHPStan\Reflection\MissingPropertyFromReflectionException
+     */
+    private function inferTypeFromController($controllerClass, Variable $node): TypeNode
+    {
+        /** @var Scope|null $scope */
+        $scope = $node->getAttribute(AttributeKey::SCOPE);
+
+        $propertyName = $node->name;
+
+        // XXX ondrey hinted that ClassReflection::getNativeProperty() might be enough
+        // https://github.com/phpstan/phpstan/discussions/4837
+        $classReflection = $this->reflectionProvider->getClass($controllerClass);
+        $propertyReflection = $classReflection->getProperty($propertyName, $scope);
+
+        return $this->staticTypeMapper->mapPHPStanTypeToPHPStanPhpDocTypeNode($propertyReflection->getReadableType());
     }
 
     /*
