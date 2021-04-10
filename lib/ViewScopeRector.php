e6b0d26bd730ac10f1a222302c0f62092ad2409d
Markus Staab <markus.staab@redaxo.de>
2021-04-10 22:46:43 +0200

    use scope from ast node

--- lib/ViewScopeRector.php
+++ lib/ViewScopeRector.php
@@ -4,6 +4,7 @@ namespace ViewScopeRector;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr\Variable;
+use PHPStan\Analyser\Scope;
 use PHPStan\PhpDocParser\Ast\PhpDoc\ParamTagValueNode;
 use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocNode;
 use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocTagNode;
@@ -14,6 +15,7 @@ use PHPStan\PhpDocParser\Ast\Type\NullableTypeNode;
 use PHPStan\PhpDocParser\Ast\Type\TypeNode;
 use PHPStan\Reflection\ReflectionProvider;
 use Rector\Core\Rector\AbstractRector;
+use Rector\NodeTypeResolver\Node\AttributeKey;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\ConfiguredCodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
@@ -61,14 +63,16 @@ array(
             return null;
         }
 
-        $inferredType = $this->inferTypeFromController("\IndexController", $node->exprs[0]->name);
+        $variable = $node->exprs[0];
+
+        $inferredType = $this->inferTypeFromController("\IndexController", $variable);
         if (!$inferredType) {
             return null;
         }
 
         // https://github.com/rectorphp/rector/blob/main/docs/how_to_work_with_doc_block_and_comments.md
         $phpDocInfo = $this->phpDocInfoFactory->createFromNodeOrEmpty($node);
-        $phpDocInfo->addTagValueNode(new VarTagValueNode($inferredType, '$' . $node->exprs[0]->name, ''));
+        $phpDocInfo->addTagValueNode(new VarTagValueNode($inferredType, '$' . $variable->name, ''));
 
         return $node;
     }
@@ -78,13 +82,17 @@ array(
      * @param string $propertyName
      * @throws \PHPStan\Reflection\MissingPropertyFromReflectionException
      */
-    private function inferTypeFromController($controllerClass, $propertyName): TypeNode
+    private function inferTypeFromController($controllerClass, Variable $node): TypeNode
     {
-        $classReflection = $this->reflectionProvider->getClass($controllerClass);
-        // XXX where to get the $scope from?
-        $propertyReflection = $classReflection->getProperty($propertyName);
+        /** @var Scope|null $scope */
+        $scope = $node->getAttribute(AttributeKey::SCOPE);
 
-        return $propertyReflection->getReadableType();
+        $propertyName = $node->name;
+
+        $classReflection = $this->reflectionProvider->getClass($controllerClass);
+        $propertyReflection = $classReflection->getProperty($propertyName, $scope);
+
+        return $this->staticTypeMapper->mapPHPStanTypeToPHPStanPhpDocTypeNode($propertyReflection->getReadableType());
     }
 
     /*
