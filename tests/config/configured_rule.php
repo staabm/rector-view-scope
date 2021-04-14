2048c99ad7d0a87d10ae32fb4ff98d1b3dbd845c
Markus Staab <m.staab@complex-it.de>
2021-04-14 16:42:11 +0200

    repro

--- tests/config/configured_rule.php
+++ tests/config/configured_rule.php
@@ -3,9 +3,13 @@
 declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Rector\Core\Configuration\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
+    $parameters = $containerConfigurator->parameters();
 
     $services->set(\ViewScopeRector\ViewScopeRector::class);
+
+    $parameters->set(Option::AUTOLOAD_PATHS, [__DIR__.'/../../myautoload.php']);
 };
