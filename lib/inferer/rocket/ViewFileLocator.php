6489faabe91ffd3edcd9fc49dab2653cecd93f7b
Markus Staab <markus.staab@redaxo.de>
2021-05-16 10:48:06 +0200

    added ViewFileLocator (#32)

---
+++ lib/inferer/rocket/ViewFileLocator.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace ViewScopeRector\Inferer\Rocket;
+
+use PhpParser\Node\Expr\Variable;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class ViewFileLocator implements \ViewScopeRector\Inferer\Rocket\FileLocator
+{
+    /**
+     * @var string
+     */
+    private $filePath;
+
+    public function __construct(string $filePath) {
+        // normalize path
+        $this->filePath = str_replace(['/', '\\'], DIRECTORY_SEPARATOR, $filePath);
+    }
+
+    public function isInViewPath(): bool
+    {
+        return strpos($this->filePath, DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR) !== false;
+    }
+
+    public function isTopLevelView(): bool
+    {
+        if (!$this->isInViewPath()) {
+            return false;
+        }
+
+        return strpos($this->filePath, DIRECTORY_SEPARATOR.'_') === false;
+    }
+
+    public function findMatchingController(): ?string
+    {
+        if (!$this->isTopLevelView()) {
+            return null;
+        }
+
+        $viewRootName = DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR;
+        $pos = strpos($this->filePath, $viewRootName);
+
+        $viewPath = substr($this->filePath, $pos + strlen($viewRootName));
+        $pos = strpos($viewPath, DIRECTORY_SEPARATOR);
+
+        if ($pos) {
+            $controllerName = substr($viewPath, 0, $pos);
+            return '\\'.ucfirst($controllerName).'Controller';
+        }
+
+        return null;
+    }
+}
--- tests/rector/config/configured_rule.php
+++ tests/rector/config/configured_rule.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Rector\Core\Configuration\Option;
+use ViewScopeRector\Inferer\Rocket\TestFileLocator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
     $parameters = $containerConfigurator->parameters();
 
-    $services->set(\ViewScopeRector\ViewScopeRector::class);
+    $services->set(\ViewScopeRector\ViewScopeRector::class)
+        ->call('configure', [[
+            \ViewScopeRector\ViewScopeRector::LOCATOR_CLASS => TestFileLocator::class,
+        ]]);
 
     $parameters->set(Option::BOOTSTRAP_FILES, [ __DIR__.'/../myautoload.php']);
 };
